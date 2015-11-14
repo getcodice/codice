411debb5384c5cedd4d1eee09aa87cd8279ae035
Sobak <msobaczewski@gmail.com>
2015-11-14 22:56:20 +0100

    Mobile styling for note buttons

--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -8,9 +8,9 @@
     <div class="panel-footer">
         <span data-toggle="tooltip" title="{{ $note->created_at->format('d.m.Y H:i') }}">@icon('clock-o') {{ $note->created_at->diffForHumans() }}</span>
         <span class="pull-right hidden-print note-buttons">
-             <a href="">@icon('check') @lang('note.buttons.done')</a>
-             <a href="">@icon('pencil') @lang('note.buttons.edit')</a>
-             <a href="" class="confirm-deletion">@icon('trash-o') @lang('note.buttons.remove')</a>
+             <a href="">@icon('check') <span class="text">@lang('note.buttons.done')</span></a>
+             <a href="">@icon('pencil') <span class="text">@lang('note.buttons.edit')</span></a>
+             <a href="" class="confirm-deletion">@icon('trash-o') <span class="text">@lang('note.buttons.remove')</span></a>
         </span>
     </div>
 </article>
