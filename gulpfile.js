c175c50c56a5d4f4e95912a6357a53289eb25993
Sobak <msobaczewski@gmail.com>
2015-11-18 12:02:22 +0100

    Better directories structure

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,12 @@ gulp.task('scripts', function () {
     return gulp.src([
         './bower_components/jquery/dist/jquery.min.js',
         './bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
-        './resources/scripts/codice.js',
+        './resources/assets/scripts/codice.js',
     ]).pipe(gulp.dest('./public/assets/js'));
 });
 
 gulp.task('styles', function () {
-    return gulp.src('./resources/styles/codice.scss')
+    return gulp.src('./resources/assets/styles/codice.scss')
         .pipe(plugins.sass({
             includePaths: [
                 './bower_components/bootstrap-sass/assets/stylesheets/',
