31a812a5879609a3f415b4c8ed7c9205ffaf46cd
Sobak <msobaczewski@gmail.com>
2015-12-05 00:11:41 +0100

    Justify paragraph about Codice

--- resources/views/info/about.blade.php
+++ resources/views/info/about.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <h2>Codice <small>{{ $version }}</small></h2>
 
-<p class="lead"><strong>Codice</strong> @lang('info.about.about-codice')</p>
+<p class="lead text-justify"><strong>Codice</strong> @lang('info.about.about-codice')</p>
 
 <div class="row">
     <textarea class="col-md-8" rows="7" disabled>{{ $changelog }}</textarea>
