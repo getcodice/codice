ce205325aabe8f7537a0c06d76f9de92f66ea46e
Sobak <msobaczewski@gmail.com>
2015-12-05 06:04:00 +0100

    Improve textarea showing changelog

--- resources/views/info/about.blade.php
+++ resources/views/info/about.blade.php
@@ -6,7 +6,9 @@
 <p class="lead text-justify"><strong>Codice</strong> @lang('info.about.about-codice')</p>
 
 <div class="row">
-    <textarea class="col-md-8" rows="7" disabled>{{ $changelog }}</textarea>
+    <div class="col-md-8">
+        <textarea class="form-control" rows="7" style="resize:none" readonly>{{ $changelog }}</textarea>
+    </div>
     <div class="col-md-4">
         <a href="{!! route('about.updates') !!}" class="btn btn-success btn-block">
             @lang('info.updates.check')
