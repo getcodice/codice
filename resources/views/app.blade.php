6c6d17cffcdbc44c385ace097da0423498188154
Sobak <msobaczewski@gmail.com>
2015-12-08 07:14:54 +0100

    We still need that font style for p.lead

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>{{ isset($title) ? $title . ' • ' : '' }}Codice</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic&amp;subset=latin,latin-ext">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic&amp;subset=latin,latin-ext">
     <link rel="stylesheet" href="{!! asset('assets/css/codice.css') !!}">
 </head>
 <body>
