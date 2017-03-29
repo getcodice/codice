8882c96e8652ea000d7dc4369a54230d371bd74e
Sobak <msobaczewski@gmail.com>
2017-03-29 13:06:32 +0200

    Increase test coverage

---
+++ tests/functional/NoteControllerTest.php
@@ -0,0 +1,87 @@
+<?php
+
+use Codice\Note;
+use Codice\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class NoteControllerTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testDisplayingIndex()
+    {
+        $this->setUpTest();
+
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function testDisplayingNote()
+    {
+        $this->setUpTest();
+
+        factory(Note::class)->create(['user_id' => 1]);
+
+        $response = $this->get('note/1');
+
+        $response->assertStatus(200);
+    }
+
+    public function testDisplayingMissingNote()
+    {
+        $this->setUpTest();
+
+        $response = $this->get('/note/5');
+
+        $response->assertStatus(302);
+    }
+
+    public function testDisplayingNoteCreationPage()
+    {
+        $this->setUpTest();
+
+        $response = $this->get('/create');
+
+        $response->assertStatus(200);
+    }
+
+    public function testDisplayingNoteEditionPage()
+    {
+        $this->setUpTest();
+
+        $response = $this->get('/note/1/edit');
+
+        $response->assertStatus(200);
+    }
+
+    public function testTogglingNote()
+    {
+        $this->setUpTest();
+
+        $this->get('/note/1/toggle');
+
+        // Welcome note is marked as done by default
+        $this->assertDatabaseHas('notes', [
+            'id' => 1,
+            'status' => 0
+        ]);
+    }
+
+    public function testRemovingNote()
+    {
+        $this->setUpTest();
+
+        $this->get('/note/1/remove');
+
+        $this->assertNull(Note::first());
+    }
+
+    private function setUpTest()
+    {
+        factory(User::class)->create();
+        Auth::loginUsingId(1);
+
+        // Note is inserted automagically due to codice_add_welcome_note action
+    }
+}
--- tests/unit/CodiceTest.php
+++ tests/unit/CodiceTest.php
@@ -7,20 +7,40 @@ class CodiceTest extends TestCase
 {
     private $codice;
 
-    public function setUp()
+    public function testGettingCodiceVersion()
     {
-        parent::setUp();
+        $this->setCodiceVersion('test-version');
 
+        $this->assertEquals('test-version', $this->codice->getVersion());
+    }
+
+    public function testIsVersionStable()
+    {
+        $this->setCodiceVersion('1.0.0');
+        $this->assertTrue($this->codice->isVersionStable());
+
+        $this->setCodiceVersion('0.5.0-dev');
+        $this->assertFalse($this->codice->isVersionStable());
+    }
+
+    public function testVersionFilter()
+    {
+        $this->setCodiceVersion('test-version');
+
+        Filter::register('core.version', 'test_filter', function ($value) {
+            return strtoupper($value);
+        });
+
+        $this->assertEquals('TEST-VERSION', $this->codice->getVersion());
+    }
+
+    private function setCodiceVersion($version)
+    {
         $this->codice = new Codice;
 
         $reflection = new ReflectionClass($this->codice);
         $reflectionVersion = $reflection->getProperty('version');
         $reflectionVersion->setAccessible(true);
-        $reflectionVersion->setValue($this->codice, 'test-version');
-    }
-
-    public function testGettingCodiceVersion()
-    {
-        $this->assertEquals('test-version', $this->codice->getVersion());
+        $reflectionVersion->setValue($this->codice, $version);
     }
 }
