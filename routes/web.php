1bd9894269ef311d585962d481c8fe319516f789
Sobak <msobaczewski@gmail.com>
2017-04-01 10:51:58 +0200

    Use more concise syntax for defining routes

--- routes/web.php
+++ routes/web.php
@@ -11,66 +11,66 @@
 |
 */
 
-Route::get('/', ['as' => 'index', 'uses' => 'NoteController@getIndex']);
+Route::get('/', 'NoteController@getIndex')->name('index');
 
-Route::get('about', ['as' => 'about', 'uses' => 'InfoController@getAbout']);
-Route::get('about/check-updates', ['as' => 'about.updates', 'uses' => 'InfoController@getUpdates']);
+Route::get('about', 'InfoController@getAbout')->name('about');
+Route::get('about/check-updates', 'InfoController@getUpdates')->name('about.updates');
 
-Route::get('calendar', ['as' => 'calendar', 'uses' => 'CalendarController@getIndex']);
-Route::get('calendar/{year}-{month}', ['as' => 'calendar.month', 'uses' => 'CalendarController@getMonth'])
+Route::get('calendar', 'CalendarController@getIndex')->name('calendar');
+Route::get('calendar/{year}-{month}', 'CalendarController@getMonth')->name('calendar.month')
     ->where(['year' => '[0-9]{4}', 'month' => '[0-9]{2}']);
-Route::get('calendar/{year}-{month}-{day}', ['as' => 'calendar.day', 'uses' => 'CalendarController@getDay'])
+Route::get('calendar/{year}-{month}-{day}', 'CalendarController@getDay')->name('calendar.day')
     ->where(['year' => '[0-9]{4}', 'month' => '[0-9]{2}', 'day' => '[0-9]{2}']);
 
-Route::get('create', ['as' => 'note.create', 'uses' => 'NoteController@getCreate']);
+Route::get('create', 'NoteController@getCreate')->name('note.create');
 Route::post('create', 'NoteController@postCreate');
 
 Route::get('install', 'InstallController@getWelcome');
-Route::get('install/requirements', ['as' => 'install.requirements', 'uses' => 'InstallController@getRequirements']);
-Route::get('install/env', ['as' => 'install.environment', 'uses' => 'InstallController@getEnvironment']);
+Route::get('install/requirements', 'InstallController@getRequirements')->name('install.requirements');
+Route::get('install/env', 'InstallController@getEnvironment')->name('install.environment');
 Route::post('install/env', 'InstallController@postEnvironment');
-Route::get('install/database/{lang}', ['as' => 'install.database', 'uses' => 'InstallController@getDatabase']);
-Route::get('install/user', ['as' => 'install.user', 'uses' => 'InstallController@getUser']);
+Route::get('install/database/{lang}', 'InstallController@getDatabase')->name('install.database');
+Route::get('install/user', 'InstallController@getUser')->name('install.user');
 Route::post('install/user', 'InstallController@postUser');
-Route::get('install/change-language/{lang}', ['as' => 'install.language', 'uses' => 'InstallController@getChangeLanguage']);
+Route::get('install/change-language/{lang}', 'InstallController@getChangeLanguage')->name('install.language');
 
-Route::get('login', ['as' => 'user.login', 'uses' => 'UserController@getLogin']);
+Route::get('login', 'UserController@getLogin')->name('user.login');
 Route::post('login', 'UserController@postLogin');
-Route::get('logout', ['as' => 'user.logout', 'uses' => 'UserController@getLogout']);
+Route::get('logout', 'UserController@getLogout')->name('user.logout');
 
-Route::get('note/{id}', ['as' => 'note', 'uses' => 'NoteController@getNote']);
-Route::get('note/{id}/edit', ['as' => 'note.edit', 'uses' => 'NoteController@getEdit']);
+Route::get('note/{id}', 'NoteController@getNote')->name('note');
+Route::get('note/{id}/edit', 'NoteController@getEdit')->name('note.edit');
 Route::post('note/{id}/edit', 'NoteController@postEdit');
-Route::get('note/{id}/remove', ['as' => 'note.remove', 'uses' => 'NoteController@getRemove']);
-Route::get('note/{id}/remove/confirm', ['as' => 'note.remove.confirm', 'uses' => 'NoteController@getRemoveConfirm']);
-Route::get('note/{id}/toggle', ['as' => 'note.toggle', 'uses' => 'NoteController@getToggle']);
+Route::get('note/{id}/remove', 'NoteController@getRemove')->name('note.remove');
+Route::get('note/{id}/remove/confirm', 'NoteController@getRemoveConfirm')->name('note.remove.confirm');
+Route::get('note/{id}/toggle', 'NoteController@getToggle')->name('note.toggle');
 
-Route::get('labels', ['as' => 'labels', 'uses' => 'LabelController@getIndex']);
-Route::get('labels/create', ['as' => 'label.create', 'uses' => 'LabelController@getCreate']);
+Route::get('labels', 'LabelController@getIndex')->name('labels');
+Route::get('labels/create', 'LabelController@getCreate')->name('label.create');
 Route::post('labels/create', 'LabelController@postCreate');
-Route::get('label/{id}', ['as' => 'label', 'uses' => 'LabelController@getNotes']);
-Route::get('label/{id}/edit', ['as' => 'label.edit', 'uses' => 'LabelController@getEdit']);
+Route::get('label/{id}', 'LabelController@getNotes')->name('label');
+Route::get('label/{id}/edit', 'LabelController@getEdit')->name('label.edit');
 Route::post('label/{id}/edit', 'LabelController@postEdit');
-Route::get('label/{id}/remove', ['as' => 'label.remove', 'uses' => 'LabelController@getRemove']);
+Route::get('label/{id}/remove', 'LabelController@getRemove')->name('label.remove');
 
-Route::get('plugin/{id}/disable', ['as' => 'plugin.disable', 'uses' => 'PluginController@getDisable']);
-Route::get('plugin/{id}/enable', ['as' => 'plugin.enable', 'uses' => 'PluginController@getEnable']);
-Route::get('plugin/{id}/install', ['as' => 'plugin.install', 'uses' => 'PluginController@getInstall']);
-Route::get('plugin/{id}/remove', ['as' => 'plugin.remove', 'uses' => 'PluginController@getRemove']);
-Route::get('plugin/{id}/uninstall', ['as' => 'plugin.uninstall', 'uses' => 'PluginController@getUninstall']);
-Route::get('plugins', ['as' => 'plugins', 'uses' => 'PluginController@getIndex']);
+Route::get('plugins', 'PluginController@getIndex')->name('plugins');
+Route::get('plugin/{id}/disable', 'PluginController@getDisable')->name('plugin.disable');
+Route::get('plugin/{id}/enable', 'PluginController@getEnable')->name('plugin.enable');
+Route::get('plugin/{id}/install', 'PluginController@getInstall')->name('plugin.install');
+Route::get('plugin/{id}/remove', 'PluginController@getRemove')->name('plugin.remove');
+Route::get('plugin/{id}/uninstall', 'PluginController@getUninstall')->name('plugin.uninstall');
 
-Route::get('password/email', ['as' => 'password.email', 'uses' => 'UserController@getEmail']);
+Route::get('password/email', 'UserController@getEmail')->name('password.email');
 Route::post('password/email', 'UserController@postEmail');
-Route::get('password/reset/{token}', ['as' => 'password.reset', 'uses' => 'UserController@getReset']);
+Route::get('password/reset/{token}', 'UserController@getReset')->name('password.reset');
 Route::post('password/reset', 'UserController@postReset');
 
-Route::get('reminders', ['as' => 'reminders', 'uses' => 'ReminderController@getIndex']);
-Route::get('reminder/{id}/remove', ['as' => 'reminder.remove', 'uses' => 'ReminderController@getRemove']);
+Route::get('reminders', 'ReminderController@getIndex')->name('reminders');
+Route::get('reminder/{id}/remove', 'ReminderController@getRemove')->name('reminder.remove');
 
-Route::get('search', ['as' => 'search', 'uses' => 'SearchController@getIndex']);
+Route::get('search', 'SearchController@getIndex')->name('search');
 
-Route::get('settings', ['as' => 'settings', 'uses' => 'UserController@getSettings']);
+Route::get('settings', 'UserController@getSettings')->name('settings');
 Route::post('settings', 'UserController@postSettings');
 
-Route::get('stats', ['as' => 'stats', 'uses' => 'InfoController@getStats']);
+Route::get('stats', 'InfoController@getStats')->name('stats');
